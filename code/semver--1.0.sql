00f1ad89d8ca652126afd7893ee7255fa961dbf4
abeehha <bscs24010@itu.edu.pk>
2026-05-14 15:47:32 +0500

    .sql phase 2 file

--- code/semver--1.0.sql
+++ code/semver--1.0.sql
@@ -16,3 +16,120 @@ CREATE TYPE semver (
     INTERNALLENGTH = 12,
     ALIGNMENT = int4
 );
+
+CREATE FUNCTION semver_lt(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_lt'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_le(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_le'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_eq(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_eq'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_ne(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_ne'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_ge(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_ge'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_gt(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'semver_gt'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION semver_cmp(semver, semver)
+RETURNS int4
+AS 'MODULE_PATHNAME', 'semver_cmp'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE OPERATOR < (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_lt,
+    COMMUTATOR = >,
+    NEGATOR = >=
+);
+
+CREATE OPERATOR <= (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_le,
+    COMMUTATOR = >=,
+    NEGATOR = >
+);
+
+CREATE OPERATOR = (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_eq,
+    COMMUTATOR = =,
+    NEGATOR = <>
+);
+
+CREATE OPERATOR <> (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_ne,
+    COMMUTATOR = <>,
+    NEGATOR = =
+);
+
+CREATE OPERATOR >= (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_ge,
+    COMMUTATOR = <=,
+    NEGATOR = <
+);
+
+CREATE OPERATOR > (
+    LEFTARG = semver,
+    RIGHTARG = semver,
+    PROCEDURE = semver_gt,
+    COMMUTATOR = <,
+    NEGATOR = <=
+);
+
+CREATE FUNCTION major(semver)
+RETURNS int4
+AS 'MODULE_PATHNAME', 'major'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION minor(semver)
+RETURNS int4
+AS 'MODULE_PATHNAME', 'minor'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION patch(semver)
+RETURNS int4
+AS 'MODULE_PATHNAME', 'patch'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION bump_minor(semver)
+RETURNS semver
+AS 'MODULE_PATHNAME', 'bump_minor'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION is_compatible(semver, semver)
+RETURNS bool
+AS 'MODULE_PATHNAME', 'is_compatible'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE OPERATOR CLASS semver_ops
+DEFAULT FOR TYPE semver USING btree AS
+    OPERATOR 1 <,
+    OPERATOR 2 <=,
+    OPERATOR 3 =,
+    OPERATOR 4 >=,
+    OPERATOR 5 >,
+    FUNCTION 1 semver_cmp(semver, semver);
